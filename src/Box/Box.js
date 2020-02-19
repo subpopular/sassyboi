afe86c99036aca40d005c1c8f1b9cc215d2dd73a
Darek Rossman <drossman@64labs.com>
2020-02-19 11:15:10 -0500

    more

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import t from 'prop-types'
 import cx from 'classnames'
 import {renderBackgroundProvider} from '../util/BackgroundContext'
 import useBoxStyles from './useBoxStyles'
@@ -65,5 +66,42 @@ const Box = React.forwardRef(
     return renderBackgroundProvider(background, element)
   }
 )
+
+const spacingProps = [
+  'xxsmall',
+  'xsmall',
+  'smallish',
+  'small',
+  'gutter',
+  'medium',
+  'large',
+  'largeish',
+  'xlarge',
+  'xxlarge'
+]
+
+Box.propTypes = {
+  as: t.element,
+  background: t.oneOf(['brand', 'critical']),
+  padding: t.oneOf(spacingProps),
+  paddingTop: t.oneOf(spacingProps),
+  paddingRight: t.oneOf(spacingProps),
+  paddingBottom: t.oneOf(spacingProps),
+  paddingLeft: t.oneOf(spacingProps),
+  paddingX: t.oneOf(spacingProps),
+  paddingY: t.oneOf(spacingProps),
+  margin: t.oneOf(spacingProps),
+  marginTop: t.oneOf(spacingProps),
+  marginRight: t.oneOf(spacingProps),
+  marginBottom: t.oneOf(spacingProps),
+  marginLeft: t.oneOf(spacingProps),
+  marginX: t.oneOf(spacingProps),
+  marginY: t.oneOf(spacingProps),
+  boxShadow: t.string,
+  display: t.string,
+  flexGrow: t.number,
+  size: t.number,
+  className: t.string
+}
 
 export default Box
