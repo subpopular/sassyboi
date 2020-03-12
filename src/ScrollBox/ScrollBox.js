c434d203deecfb734b582070cd97a12797663dd7
willwarren64 <wwarren@64labs.com>
2020-03-12 16:40:42 -0400

    Box component expansion added scrollX and scrollY props to ScrollBox to use new overflow props on Box component

--- src/ScrollBox/ScrollBox.js
+++ src/ScrollBox/ScrollBox.js
@@ -3,12 +3,12 @@ import cx from 'classnames'
 import Box from '../Box/Box'
 import './scrollbox.css'
 
-const ScrollBox = ({className, children, ...props}) => {
+const ScrollBox = ({className, scrollX = false, scrollY = false, children, ...props}) => {
   const classes = cx('u-scrollbox', className)
 
   return (
     <Box className={classes} {...props}>
-      <Box className='u-scrollbox__wrapper'>
+      <Box overflowX={scrollX ? 'scroll' : undefined} overflowY={scrollY ? 'scroll' : undefined} className='u-scrollbox__wrapper'>
         <Box className='u-scrollbox__container'>{children}</Box>
       </Box>
     </Box>
